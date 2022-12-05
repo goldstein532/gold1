3c63b2d6d2597abcc73d988464f6fb9ce476f117
Gold! <pluxurygold532@gmail.com>
2022-12-05 18:06:18 +0300

    Create Использование контейнеров.cpp

---
+++ Использование контейнеров.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool isLol(int x) {
+    return (x > a && x < b);
+}
+int main() {
+    vector <int> v;
+    int n, a, b, x;
+    cout << "n= "; cin >> n;
+    cout << endl << "a,b= "; cin >> a; cin >> b;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        v.push_back(x);
+    }
+    replace_if(v.begin(), v.end(), isLol, 0);
+    for (vector <int>::iterator iter = v.begin(); iter < v.end(); iter++)
+        cout << *iter << ' ';
+    system("pause");
+    return 0;
+}
