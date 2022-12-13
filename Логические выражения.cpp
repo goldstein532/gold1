018d967eefa37fd7d7a877e8344a9fa02ece1f34
Gold! <pluxurygold532@gmail.com>
2022-12-13 15:07:05 +0300

    Create Логические выражения.cpp

---
+++ Логические выражения.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int A, B, C;
+	cout << "A:";
+	cin >> A;
+	cout << "B:";
+	cin >> B;
+	cout << "C:";
+	cin >> C;
+
+	if (B > A && B < C || B<A && B>C) {
+		cout << "Истинно";
+	}
+	else {
+		cout << "Ложь";
+	}
+}
