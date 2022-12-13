c66c7b2462583589828bc4d110cd065af1b4cbf9
Gold! <pluxurygold532@gmail.com>
2022-12-13 15:10:32 +0300

    Create Целые числа.cpp

---
+++ Целые числа.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int a, b, c;
+    int sum, i;
+    cout << "Двухзначное число:" << endl;
+    cin >> a;
+
+    c = a / 10;
+    b = a % 10;
+
+    sum = c + b;
+    cout << "Сумма чисел" << sum << endl;
+
+    i = c * b;
+    cout << "Произведение чисел:" << i;
+}
