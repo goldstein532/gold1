2b47a9598c18394f089a8f7fc97618836d250dba
Gold! <pluxurygold532@gmail.com>
2022-12-15 23:23:55 +0300

    Create лог выр 2.cpp

---
+++ лог выр 2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int A, B, C;
+	cout << "A:";
+	cin >> A;
+	cout << "B:";
+	cin >> B;
+	cout << "C:";
+	cin >> C;
+
+	B > A&& B < C || B<A&& B>C ? cout << "Истинно" : cout << "Ложь";
