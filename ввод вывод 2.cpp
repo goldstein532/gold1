053bbf9feebcbdf1f6456b0f221036b8004f6ee7
Gold! <pluxurygold532@gmail.com>
2022-12-15 22:49:43 +0300

    Create ввод вывод 2.cpp

---
+++ ввод вывод 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <float.h>
+
+using namespace std;
+
+#include <stdio.h>
+
+int main(void)
+{
+    float R;
+    printf("R:");
+    scanf("%f", &R);
+
+    float L = 2 * 3.14 * R;
+    printf("V:%f\n", L);
+
+    float S = 3.14 * R * R;
+    printf("S:%f\n", S);
+
+    return 0;
+}
