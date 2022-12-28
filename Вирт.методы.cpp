54c9997eae11b1652f4a116131c75da6df0dfe49
Gold! <pluxurygold532@gmail.com>
2022-12-28 21:21:49 +0300

    Create Вирт.методы.cpp

---
+++ Вирт.методы.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+class Hex
+{
+public:
+	virtual void insert() = 0;
+
+	virtual void write() = 0;
+private:
+	unsigned char chislo[100];
+
+};
+class Chislohex : public Hex {
+public:
+	void insert() {
+		cout << "Write kolichestvo cifor ";
+
+		cin >> length;
+		cout << "Write chislo v HEX: ";
+		string chislohexin;
+		cin >> chislohexin;
+		for (int i = 0; i < length; i++)
+		{
+			chislo[i] = chislohexin[i];
+		}
+
+	}
+	void write() {
+		for (int i = 0; i < length; i++)
+		{
+			cout << chislo[i];
+		}
+	}
+private:
+	unsigned char chislo[100];
+	int length;
+};
+int main() {
+	Chislohex new1;
+	new1.insert();
+	new1.write();
+}
