01925af9abceaada93c64afb8683d03fa0059f3c
Gold! <pluxurygold532@gmail.com>
2022-12-29 20:41:16 +0300

    Create ВИРТУАЛЬНЫЕ МЕТОДЫ 7.cpp

---
+++ ВИРТУАЛЬНЫЕ МЕТОДЫ 7.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+using namespace std;
+class Oct
+{
+public:
+    virtual void insert() = 0;
+
+    virtual void write() = 0;
+private:
+    unsigned char chislo[100];
+
+};
+class ChisloOct : public Oct {
+public:
+    void insert() {
+        cout << "Write kolichestvo cifor ";
+
+        cin >> length;
+        cout << "Write chislo v OCT: ";
+        string chisloOctin;
+        cin >> chisloOctin;
+        for (int i = 0; i < length; i++)
+        {
+            chislo[i] = chisloOctin[i];
+        }
+
+    }
+    void write() {
+        for (int i = 0; i < length; i++)
+        {
+            cout << chislo[i];
+        }
+    }
+private:
+    unsigned char chislo[100];
+    int length;
+};
+int main() {
+    ChisloOct new1;
+    new1.insert();
+    new1.write();
+}
+
+class BitString
+{
+public:
+    virtual void insert() = 0;
+
+    virtual void write() = 0;
+private:
+    unsigned char chislo[100];
+
+};
+class BitStringChislo : public BitString {
+public:
+    void insert() {
+        cout << "Write kolichestvo cifor ";
+
+        cin >> length;
+        cout << "Write Sring by bit: ";
+        string chisloOctin;
+        cin >> chisloOctin;
+        for (int i = 0; i < length; i++)
+        {
+            chislo[i] = chisloOctin[i];
+        }
+
+    }
+    void write() {
+        for (int i = 0; i < length; i++)
+        {
+            cout << chislo[i];
+        }
+    }
+private:
+    unsigned char chislo[100];
+    int length;
+};
+int main() {
+    BitStringChislo new1;
+    new1.insert();
+    new1.write();
+}
